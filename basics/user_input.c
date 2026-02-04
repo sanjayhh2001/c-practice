80b17707326222534d4f3a94988b502a17beffe1
Sanjay H H <sanjayhh2001@gmail.com>
2026-02-04 07:32:20 +0530

    user input

---
+++ basics/user_input.c
@@ -0,0 +1,15 @@
+// Write a C program to read an integer from the user and print it.
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+
+    printf("Enter the value of a: ");
+    scanf("%d",&a);
+    
+    printf("The entered value of a is: %d",a);
+
+    return 0;
+}
