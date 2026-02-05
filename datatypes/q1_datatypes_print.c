80ade5d52a2f1c395608b259ed96b77139ad4c86
Sanjay UB <sanjayhh2001@gmail.com>
2026-02-05 16:48:01 +0530

    Added datatypes programs

---
+++ datatypes/q1_datatypes_print.c
@@ -0,0 +1,18 @@
+//Declare one int, float, char, and double variable, assign values, and print them.
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 19; 
+    float b = 3.145;
+    char ch = 'S';
+    double d = 3.158874;
+
+    printf("The integer value is: %d\n",a);
+    printf("The float value is: %f\n",b);
+    printf("The character value is: %c\n",ch);
+    printf("The double value is: %lf\n",d);
+    
+    return 0;
+}
