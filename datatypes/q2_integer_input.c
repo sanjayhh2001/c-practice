f242922088b8b4104a78bb5b9ec6be8449a82ed4
Sanjay UB <sanjayhh2001@gmail.com>
2026-02-05 17:01:14 +0530

    Added integer user input datatype program

---
+++ datatypes/q2_integer_input.c
@@ -0,0 +1,15 @@
+//Take an integer input from the user and print it.
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+
+    printf("Enter the value of a: ");
+    scanf("%d",&a);
+
+    printf("The enter value of a is: %d\n",a);
+    
+    return 0;
+}
